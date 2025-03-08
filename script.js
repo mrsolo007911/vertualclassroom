5a8fdc73bce529c89cd3522b2dbb85db7f71b3cf
shivam yadav <7.unofficial.shiv@gmail.com>
2025-03-08 16:18:48 +0530

    added login button

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for interactivity
+document.getElementById('cta-button').addEventListener('click', function() {
+    alert('Welcome to the Virtual Classroom! Let\'s get started.');
+});
+
+// You can add more JavaScript functionality as needed
